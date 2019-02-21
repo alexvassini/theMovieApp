d404cbc0391b56310fb40a9d29ab701835ca4914
Alexandre Vassinievski Ribeiro <AlexandreVassinievski@311PDES049343N.local>
2019-02-20 21:38:45 -0300

    first commit

---
+++ TheMovieApp/TheMovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMovieApp
+//
+//  Created by Alexandre Vassinievski Ribeiro on 20/02/19.
+//  Copyright © 2019 Alexandre Vassinievski Ribeiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
